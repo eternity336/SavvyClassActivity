f523f0aa5b12c9c84d94a5220cbfa3dd0fc3f254
Joshua Ochsankehl <eternity336@gmail.com>
2023-01-18 21:25:02 -0600

    Completed 2.3 Activities

---
+++ Section02/2.3/2.3.1-MathOperatorsAndDataCoercionIntro.js
@@ -0,0 +1,27 @@
+// What should we expect to print?
+// 7
+// 9
+// 0
+// 0
+// 105
+// Hello world
+// 22
+// 3
+// 2truefalse
+// 223
+
+// Basic Math
+console.log(1 + 2 * 3);
+console.log((1 + 2) * 3);
+console.log(40 / 5 - 2 ** 3 );
+console.log(6 % 2);
+
+// Adding/Concatenating Strings
+console.log("10" + "5");
+console.log("Hello " + "world");
+
+// Data Coercion
+console.log("2" + 2);
+console.log(2 + true);
+console.log(true + true + "true" + "false");
+console.log(1 + 1 + "2" + "3");
