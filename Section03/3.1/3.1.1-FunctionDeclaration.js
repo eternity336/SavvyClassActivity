f356139328792d00acbdc43abf0edf0cb44db922
Joshua Ochsankehl <eternity336@gmail.com>
2023-01-25 18:25:59 -0600

    Section 3.1 Activities

---
+++ Section03/3.1/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,17 @@
+function greet(){
+  console.log("Hello!");
+};
+
+function threeModTwo(){
+  console.log(3 % 2);
+};
+
+let age = 18;
+
+function checkDrivingAge(age){
+  if (age >= 16) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+};
---
+++ Section03/3.1/3.1.2-FunctionInvocation.js
@@ -0,0 +1,14 @@
+// print the function below
+function myNumberFunction() {
+  let num = 2 ** 2;
+  num = num + 3;
+  num = num % 4;
+  num = num * 14;
+  console.log(num);
+}
+  
+// now, invoke the myNumberFunction function
+myNumberFunction();
+
+// print just the function of console.log (no invocation)
+console.log(myNumberFunction);
